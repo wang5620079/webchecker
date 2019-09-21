e655f64e8e6af5e093bcfad5f36b53dac1a7e27c
wp <wang5620079@163.com>
2019-09-21 18:04:43 +0800

    web检查器 1、增加IE11的兼容性 2、修改了首页，增加欢迎页 3、增加content的自适应高度变化

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
     </a-layout-sider>
     <a-layout>
       <a-layout-content style="margin: 0 16px">
-        <div :style="{ padding: '24px', background: '#fff', minHeight: '720px' }">
+        <div :style="{ padding: '24px', background: '#fff',  height: fullHeight + 'px' }">
           <router-view></router-view>
         </div>
       </a-layout-content>
@@ -35,12 +35,34 @@ export default {
   data () {
     return {
       collapsed: false,
-      openKeys: ['sub1', 'sub2']
+      openKeys: ['sub1', 'sub2'],
+      fullHeight: document.documentElement.clientHeight
     }
   },
+  watch: {
+    fullHeight (val) { // 监控浏览器高度变化
+      if (!this.timer) {
+        this.fullHeight = val
+        this.timer = true
+        let that = this
+        setTimeout(function () {
+          that.timer = false
+        }, 400)
+      }
+    }
+  },
+  mounted () {
+    this.get_bodyHeight()
+  },
   methods: {
-    handleMenuClick (e) {
-      console.log('click', e)
+    get_bodyHeight () { // 动态获取浏览器高度
+      const that = this
+      window.onresize = () => {
+        return (() => {
+          window.fullHeight = document.documentElement.clientHeight
+          that.fullHeight = window.fullHeight
+        })()
+      }
     }
   }
 }
